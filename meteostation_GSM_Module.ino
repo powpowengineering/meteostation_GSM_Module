0256b623f6e452c2abe3b3851c33383e73de7862
Pavel <ku2zovpavel@gmail.com>
2021-09-22 15:29:00 +0300

    Added vbat

--- meteostation_GSM_Module.ino
+++ meteostation_GSM_Module.ino
@@ -6,6 +6,9 @@
 #define SIZE_IN_BUFF                (512)
 #define PIN_PWR_GSM					(7)
 
+#define VBAT_PIN                    (A1)
+#define NUM_SAMPLES 10
+
 typedef enum
 {
     OFF = 0,
@@ -15,6 +18,13 @@ typedef enum
 
 char inBuff[SIZE_IN_BUFF];
 char tempBuff[SIZE_IN_BUFF];
+
+float r1 = 11;
+float r2 = 30;
+float vbat = 0.0;            // calculated voltage
+
+float ReadVbat(void);
+void GSM_MODULE_switchPwr(GSM_MODULE_state state);
 
 /*char dataHexTest[] = {"at+recv=-20,10,71:31382E20392E32317C31373A34313A20307C74313D20204E414E7C48756D3D20204E414E7C503D313030372E327C566261743D362E31357C436E745F313D327C436E745F323D30"};
 char dataAsciiTest[256];
@@ -35,86 +45,83 @@ void setup()
 	
 	pinMode(PIN_PWR_GSM, OUTPUT);
 	
-	//Switch on power for GSM module
-    digitalWrite(PIN_PWR_GSM, LOW);
-/*
-unsigned int cntSym=0;
-    for(int i=0;i<32;i++)
-    {
-      if (':' == dataHexTest[i])
-        {
-            break;  
-        }
-        else
-        {
-            cntSym++;
-        }  
-    }
-Serial.println(cntSym);
-
-RAK811_hexToAscii(&dataHexTest[cntSym+1],dataAsciiTest);
-Serial.print(dataAsciiTest);
-while(1);
- */   
-	
+    // Power on GSM
+    GSM_MODULE_switchPwr(ON);
+  
+    /*
 	Serial3.println("AT");
-  delay(500);
+    delay(500);
 	Serial3.println("AT+CMGF=1");                        // Выбирает формат SMS
- delay(500);
+    delay(500);
 	Serial3.println("AT+CMGS=\"+79296232270\"");         // Отправка СМС на указанный номер
- delay(500);
+    delay(500);
 	Serial3.print("I am future GSM module for yours meteo station :)"); // Тест сообщения
- delay(500);
+    delay(500);
 	Serial3.write(26);//???
- delay(500);
+    delay(500);
+    // Power on GSM
+    GSM_MODULE_switchPwr(ON);
+    */
 }
 
 void loop()
 {
-  unsigned int cntSym=0;
-  
-  
-  // if have message from meteostation
-  if (RAK811_receiveData(inBuff, SIZE_IN_BUFF) > 0)
-	{
-		//Serial.write(Serial2.read());
-		//Serial.println(">0");
-  
-    for(int i=0;i<32;i++)
-    {
-      if (':' == inBuff[i])
-        {
-            break;  
-        }
-        else
-        {
-            cntSym++;
-        }  
-    }
+    unsigned int cntSym=0;
 
-    //clear buf
-    for (int i=0;i<SIZE_IN_BUFF;i++)
+    // if have message from meteostation
+    if (RAK811_receiveData(inBuff, SIZE_IN_BUFF) > 0)
     {
-        tempBuff[i] = 0;
-    }
-    
-    RAK811_hexToAscii(&inBuff[cntSym+1],tempBuff);
-    for (int i=0;i<SIZE_IN_BUFF;i++)
-    {
-        inBuff[cntSym+1+i]=tempBuff[i];
-        if(0 == tempBuff[i])
+        // Power on GSM
+        GSM_MODULE_switchPwr(ON);
+        
+        // Search symbol ':'
+        for(int i=0;i<32;i++)
         {
-            break;
+            if (':' == inBuff[i])
+            {
+                break;  
+            }
+            else
+            {
+                cntSym++;
+            }  
         }
+
+        //clear buff
+        for (int i=0;i<SIZE_IN_BUFF;i++)
+        {
+            tempBuff[i] = 0;
+        }
+        
+        // Convert hex to Ascii
+        RAK811_hexToAscii(&inBuff[cntSym+1],tempBuff);
+        
+        // Insert converted data into buffer
+        for (int i=0;i<SIZE_IN_BUFF;i++)
+        {
+            inBuff[cntSym+1+i]=tempBuff[i];
+            if(0 == tempBuff[i])
+            {
+                break;
+            }
+        }
+        
+        vbat = ReadVbat();
+        
+        Serial.println(inBuff);
+        delay(3000);
+        Serial3.println("AT+CMGF=1");                        // Выбирает формат SMS
+        delay(500);
+        Serial3.println("AT+CMGS=\"+79296232270\"");         // Отправка СМС на указанный номер
+        delay(500);
+        Serial3.write(inBuff); // Тест сообщения
+        delay(500);
+        Serial3.write(26);//???
+        delay(5000);
+        
+        // Power off GSM
+        GSM_MODULE_switchPwr(OFF);
     }
-    
-		Serial.println(inBuff);
-		Serial3.println("AT+CMGS=\"+79296232270\"");         // Отправка СМС на указанный номер
-		delay(500);
-		Serial3.write(inBuff); // Тест сообщения
-		delay(500);
-	  Serial3.write(26);//???
-	}
 	
     
 }
@@ -133,5 +140,26 @@ void GSM_MODULE_switchPwr(GSM_MODULE_state state)
         digitalWrite(PIN_PWR_GSM, LOW);
     }
 }
+
+
+
+float ReadVbat(void)
+{
+  float voltage;
+  float sum = 0;
+  unsigned char sample_count = 0; // current sample number
+
+      while (sample_count < NUM_SAMPLES) {
+          sum += analogRead(VBAT_PIN);
+          sample_count++;
+      }
+      // use 3.3 for a 3.3V ADC reference voltage
+      voltage = 0.99516*(sum/NUM_SAMPLES)* (3.3/1024.0)/(r1/(r1+r2));
+      // send voltage for display o n Serial Monitor
+      // divides by 6.24/6 is the calibrated voltage divide
+
+      return voltage;
+
+}//end of ReadVbat
 
 
